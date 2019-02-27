9b0519a578c7d07dd361fe246606b02776c28835
wowo <zhouyuexie@gmail.com>
2019-02-27 19:07:42 +0800

    temp: save for quick

--- src/quick.rs
+++ src/quick.rs
@@ -1,29 +1,36 @@
 use time::PreciseTime;
 use array::swap;
 
-fn quick(val: &Vec<i32>, left: usize, right: usize) -> Vec<i32> {
-    let mut value = vec![6, 2, 6, 7, 4, 7, 7, 3, 1, 6];
-    let mut mid = (left + right) / 2;
+fn quick_sort(value: &mut Vec<i32>, left: usize, right: usize) {
+    let mid = (left + right) / 2;
     let mut left_index = left;
     let mut right_index = right;
-    if value.len() == 0 {
-        return value;
-    }
 
-    while left_index <= right_index {
-        while value[left_index] < value[mid] {
+    if left_index >= right_index {
+        return;
+    }
+    while left_index < right_index {
+        while value[left_index] < value[mid] && left_index < right_index {
             left_index += 1;
         }
-        while value[right_index] > value[mid] {
+        while value[right_index] > value[mid] && left_index < right_index {
             right_index -= 1;
         }
         println!("left: {}, right: {}, array: {:?}", left_index, right_index, value);
         if left_index <= right_index  {
-            swap(&mut value, left_index, right_index);
+            swap(value, left_index, right_index);
             left_index += 1;
             right_index -= 1;
         }
     }
+    quick_sort(value, left, mid - 1);
+    quick_sort(value, mid + 1, right);
+}
+
+fn quick(val: &Vec<i32>, left: usize, right: usize) -> Vec<i32> {
+    let mut value = val.clone();
+
+    quick_sort(&mut value, left, right);
 
     value
 }
