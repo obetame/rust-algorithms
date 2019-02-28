00141b701e65628d981fc5aace327a2076afd34b
wowo <zhouyuexie@gmail.com>
2019-02-28 09:13:59 +0800

    quick: temp save

--- src/quick.rs
+++ src/quick.rs
@@ -6,14 +6,17 @@ fn quick_sort(value: &mut Vec<i32>, left: usize, right: usize) {
     let mut left_index = left;
     let mut right_index = right;
 
-    if left_index >= right_index {
+    if right_index <= left_index + 1 {
         return;
     }
-    while left_index < right_index {
-        while value[left_index] < value[mid] && left_index < right_index {
+    while left_index <= right_index {
+        println!("index: {}, mid: {}", mid, value[mid]);
+        while value[left_index] < value[mid]{
+            println!("index: {}, left: {}", left_index, value[left_index]);
             left_index += 1;
         }
-        while value[right_index] > value[mid] && left_index < right_index {
+        while value[right_index] > value[mid] {
+            println!("index: {}, right: {}", right_index, value[right_index]);
             right_index -= 1;
         }
         println!("left: {}, right: {}, array: {:?}", left_index, right_index, value);
@@ -22,9 +25,15 @@ fn quick_sort(value: &mut Vec<i32>, left: usize, right: usize) {
             left_index += 1;
             right_index -= 1;
         }
+        println!("result: {:?}", value);
     }
-    quick_sort(value, left, mid - 1);
-    quick_sort(value, mid + 1, right);
+    println!("left value: {}, right value: {}, mid value: {}", left, right, mid);
+    if left < mid - 1 {
+        quick_sort(value, left, mid - 1);
+    }
+    if right > mid {
+        quick_sort(value, mid, right);
+    }
 }
 
 fn quick(val: &Vec<i32>, left: usize, right: usize) -> Vec<i32> {
